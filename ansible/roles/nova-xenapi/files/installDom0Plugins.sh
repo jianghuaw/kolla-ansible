dee0fb17d6a6dde6e65f61d806dccf1e4ed39362
Jianghua Wang <jianghuaw@citrix.com>
2017-09-26 09:32:38 +0000

    Fix a bug for plugin copying
    This contains two changes:
    1. fixed a type on the plugin path
    2. create a temp container from nova-compute image; and copy dom0
    plugins from there; so that we can avoid the dependence on the
    nova-compute container.

--- ansible/roles/nova-xenapi/files/installDom0Plugins.sh
+++ ansible/roles/nova-xenapi/files/installDom0Plugins.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ## install dom0 plugins
+image=$1
 TMP_DIR='/tmp/plugins_dom0'
 TARGET_DIR='/etc/xapi.d/plugins/'
 SOURCE_DIR=/var/lib/kolla/venv/lib/python2.7/site-packages/os_xenapi/dom0/etc/xapi.d/plugins
@@ -9,7 +10,9 @@ fi
 
 mkdir -p $TMP_DIR
 
-docker cp nova_compute:$SOURCE_DIR/ $TMP_DIR/
+container_id=$(docker create $image)
+docker cp $container_id:$SOURCE_DIR/ $TMP_DIR/
+docker rm $container_id
 
-scp -o 'StrictHostKeyChecking=no' $TMP_DIR/plugins* root@169.254.0.1:$TARGET_DIR/
+scp -o 'StrictHostKeyChecking=no' $TMP_DIR/plugins/* root@169.254.0.1:$TARGET_DIR/
 ssh -o 'StrictHostKeyChecking=no' root@169.254.0.1 chmod +x $TARGET_DIR/*
